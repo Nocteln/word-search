6e3b30eeaa5a4476d2fea211170b4aba27c34073
comforstockag <github.comforstockag.village357@passmail.net>
2025-10-30 15:08:27 +0000

    Update main.c

--- preporcessor/src/main.c
+++ preporcessor/src/main.c
@@ -2,8 +2,8 @@
 #include <string.h>
 #include <math.h>
 #include <time.h>
-#define STB_IMAGE_IMPLEMENTATION
-#define STB_IMAGE_WRITE_IMPLEMENTATION
+//#define STB_IMAGE_IMPLEMENTATION
+//#define STB_IMAGE_WRITE_IMPLEMENTATION
 #include <stb_image.h>
 #include <stb_image_write.h>
 #include <stdio.h>
@@ -20,15 +20,24 @@ const int BOX_SIZE_THRESH_MIN = 10;
 const float EDGE_DETECTION_THRESHOLD = 8.0;
 const int THRESHOLD_VALUE_FOR_1ST_TRESH_FUNC = 230;
 
-const int THRESHOLD_FOR_TAILING_WORD_CUTTING = 30; // Coupe le mot bien
-const int THRESHOLD_PIXELS_FOR_WORD_CUTTING = 250; 
+const int THRESHOLD_FOR_TAILING_WORD_CUTTING = 30;
+const int THRESHOLD_PIXELS_FOR_WORD_CUTTING = 250;
+
+
+
+const float CONFIDENCE_THRESHOLD_TO_LABEL_AS_LETTER = 0.8;
+const int LETTER_VARIATION_TOLERANCE = 20;
 
 const int DUMP_IMAGES = 0;
 
 
 void push_box_array(struct box **array, struct box box, int *size) {
   *size +=1;
-  *array = realloc(*array, *size * sizeof(struct box));
+  void *tmp = realloc(*array, (*size) * sizeof(struct box));
+  if (tmp == NULL) {
+    printf("AAAA\n");
+  }
+  *array = tmp;
   (*array)[*size-1] = box;
 }
 
@@ -38,7 +47,7 @@ void push_vec2_array(int **array, int x, int y, int *size) {
   (*array)[*size-1] = y;
   (*array)[*size-2] = x;
 }
-/*
+
 
 int main(int argc, char *argv[]) {
   if (argc < 2) return 1;
@@ -61,9 +70,9 @@ int main(int argc, char *argv[]) {
 
   *image = (struct img){ .width = width, .height = height, .channels = channels, .img = img };
 
+
   process_image(image);
 
   return 0;
 
 }
-*/
